6f6fede35a8cd71a724fe8a22e1887e94641243e
baptiste.martin <baptiste.matin@fabrick.io>
2021-10-08 16:05:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Client, Intents } = require("discord.js");
+require('dotenv').config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+	console.log("Connected !");
+})
+
+client.login(process.env.DISCORD_TOKEN);
