f08ea2c17d1bf19098a11f4cac4f7587ae21001b
baptiste.martin <baptiste.matin@fabrick.io>
2021-10-15 14:44:21 +0200

    :memo: Change id role to name

--- events/onReactions.js
+++ events/onReactions.js
@@ -8,7 +8,7 @@ module.exports = class OnReactions {
 
     if (message.channel.name === conf.commands.admin.print.school.channel) {
       message.guild.members.fetch(user.id).then(member => {
-        member.roles.add(emoji.name === "1️⃣" ? conf.roles.epsi : conf.roles.wis);
+        member.roles.add(emoji.name === "1️⃣" ? member.guild.roles.cache.find(r => r.name === conf.roles.epsi) : member.guild.roles.cache.find(r => r.name === conf.roles.wis));
       });
 
       reaction.users.remove(user.id);
@@ -23,19 +23,20 @@ module.exports = class OnReactions {
       message.guild.members.fetch(user.id).then(member => {
         switch(emoji.name) {
           case "1️⃣":
-            return member.roles.add(conf.roles.epsib1);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsib1));
           case "2️⃣":
-            return member.roles.add(conf.roles.epsib2);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsib2));
           case "3️⃣":
-            return member.roles.add(conf.roles.epsib3);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsib3));
           case "4️⃣":
-            return member.roles.add(conf.roles.epsii1);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsii1));
           case "5️⃣":
-            return member.roles.add(conf.roles.epsii2);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsii2));
         }
       })
-      .then(member => member.roles.add(conf.roles.student))
-      .then(member => member.roles.remove(conf.roles.newStudent));
+      .then(member => member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.student)))
+      .then(member => member.roles.remove(member.guild.roles.cache.find(r => r.name === conf.roles.newStudent))
+          .catch(err => console.log(err)));
 
       reaction.users.remove(user.id);
     }
@@ -49,19 +50,20 @@ module.exports = class OnReactions {
       message.guild.members.fetch(user.id).then(member => {
         switch(emoji.name) {
           case "1️⃣":
-            return member.roles.add(conf.roles.wisb1);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wisb1));
           case "2️⃣":
-            return member.roles.add(conf.roles.wisb2);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wisb2));
           case "3️⃣":
-            return member.roles.add(conf.roles.wisb3);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wisb3));
           case "4️⃣":
-            return member.roles.add(conf.roles.wisi1);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wisi1));
           case "5️⃣":
-            return member.roles.add(conf.roles.wisi2);
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wisi2));
         }
       })
-      .then(member => member.roles.add(conf.roles.student))
-      .then(member => member.roles.remove(conf.roles.newStudent));
+      .then(member => member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.student)))
+      .then(member => member.roles.remove(member.guild.roles.cache.find(r => r.name === conf.roles.newStudent))
+          .catch(err => console.log(err)));
 
       reaction.users.remove(user.id);
     }
