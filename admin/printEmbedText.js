6f77e4540d56745e062469dc7e714c82102ec836
baptiste.martin <baptiste.matin@fabrick.io>
2021-10-25 13:59:45 +0200

    :fire: Complete rework of roles attribution

--- admin/printEmbedText.js
+++ admin/printEmbedText.js
@@ -16,57 +16,30 @@ module.exports = class PrintEmbedText extends MessageWrapper {
 
     message.delete();
     if (message.member.roles.cache.some(r => r.name === conf.roles.owner)) {
-      if (witchSection === "school") {
-        this.getRoleForSchool(message, conf);
-      } else if (witchSection === "epsi") {
-        this.getRoleForEpsi(message, conf);
-      } else if (witchSection === "wis") {
-        this.getRoleForWis(message, conf);
+      if (witchSection === "roles") {
+        this.getRoles(message, conf);
       } else if (witchSection === "rules") {
         this.getRulesMessage(message, conf);
       }
     }
   }
 
-  static getRoleForSchool(message, conf) {
-    const em = new MessageEmbed()
-    .setColor(0x7C147B)
-    .setTitle(conf.commands.admin.print.school.title)
-    .setDescription(conf.commands.admin.print.school.description);
-
-  message.channel.send({embeds: [em]})
-    .then(emMessage => emMessage.react("1️⃣")
-      .then(() => emMessage.react("2️⃣"))
-    );
-  }
-
-  static getRoleForEpsi(message, conf) {
+  static getRoles(message, conf) {
     const em = new MessageEmbed()
       .setColor(0x7C147B)
-      .setTitle(conf.commands.admin.print.epsi.title)
-      .setDescription(conf.commands.admin.print.epsi.description);
+      .setTitle(conf.commands.admin.print.roles.title)
+      .setDescription(conf.commands.admin.print.roles.description);
 
     message.channel.send({embeds: [em]})
-      .then(emMessage => emMessage.react("1️⃣")
-        .then(() => emMessage.react("2️⃣"))
-        .then(() => emMessage.react("3️⃣"))
-        .then(() => emMessage.react("4️⃣"))
-        .then(() => emMessage.react("5️⃣"))
-      );
-  }
-
-  static getRoleForWis(message, conf) {
-    const em = new MessageEmbed()
-      .setColor(0x7C147B)
-      .setTitle(conf.commands.admin.print.wis.title)
-      .setDescription(conf.commands.admin.print.wis.description);
-
-    message.channel.send({embeds: [em]})
-      .then(emMessage => emMessage.react("1️⃣")
-        .then(() => emMessage.react("2️⃣"))
-        .then(() => emMessage.react("3️⃣"))
-        .then(() => emMessage.react("4️⃣"))
-        .then(() => emMessage.react("5️⃣"))
+      .then(emMessage => emMessage.react("1️⃣") // socle
+        .then(() => emMessage.react("2️⃣")) // epsi b2
+        .then(() => emMessage.react("3️⃣")) // epsi b3
+        .then(() => emMessage.react("4️⃣")) // epsi i1
+        .then(() => emMessage.react("5️⃣")) // epsi i2
+        .then(() => emMessage.react("6️⃣")) // wis b2
+        .then(() => emMessage.react("7️⃣")) // wis b3
+        .then(() => emMessage.react("8️⃣")) // wis e1
+        .then(() => emMessage.react("9️⃣")) // wis e2
       );
   }
 
--- events/onReactions.js
+++ events/onReactions.js
@@ -2,36 +2,31 @@ module.exports = class OnReactions {
 
   constructor() { }
 
-  static getWelcomeReaction(reaction, user, conf) {
+  static getRolesReaction(reaction, user, conf) {
     const message = reaction.message;
     const emoji = reaction.emoji;
 
-    if (message.channel.name === conf.commands.admin.print.school.channel) {
-      message.guild.members.fetch(user.id).then(member => {
-        member.roles.add(emoji.name === "1️⃣" ? member.guild.roles.cache.find(r => r.name === conf.roles.epsi) : member.guild.roles.cache.find(r => r.name === conf.roles.wis));
-      });
-
-      reaction.users.remove(user.id);
-    }
-  }
-
-  static getEpsiSectionReaction(reaction, user, conf) {
-    const message = reaction.message;
-    const emoji = reaction.emoji;
-
-    if (message.channel.name === conf.commands.admin.print.epsi.channel) {
+    if (message.channel.name === conf.commands.admin.print.roles.channel) {
       message.guild.members.fetch(user.id).then(member => {
         switch(emoji.name) {
-          case "1️⃣":
-            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsib1));
-          case "2️⃣":
+          case "1️⃣": // socle num
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.socle));
+          case "2️⃣": // epsi b2
             return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsib2));
-          case "3️⃣":
+          case "3️⃣": // epsi b3
             return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsib3));
-          case "4️⃣":
+          case "4️⃣": // epsi i1
             return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsii1));
-          case "5️⃣":
+          case "5️⃣": // epsi i2
             return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.epsii2));
+          case "6️⃣": // wis b2
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wisb2));
+          case "7️⃣": // wis b3
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wisb3));
+          case "8️⃣": // wis e1
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wise1));
+          case "9️⃣": // wis e2
+            return member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.wise2));
         }
       })
       .then(member => member.roles.add(member.guild.roles.cache.find(r => r.name === conf.roles.student)))
