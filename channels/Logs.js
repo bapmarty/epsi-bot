8a6f2166d144aa65ddfdd6e5c2f2e110cd101a4f
baptiste.martin <baptiste.matin@fabrick.io>
2021-10-26 23:56:47 +0200

    :sparkles: Add Logs class for log everything on one discord channel

---
+++ channels/Logs.js
@@ -0,0 +1,18 @@
+const {MessageEmbed} = require("discord.js");
+module.exports = class Logs {
+  constructor(message, client, logMessage, color, channel) {
+    this.message = message;
+    this.client = client;
+    this.logMessage = logMessage;
+    this.color = color;
+    this.channel = this.message.guild.channels.cache.find(c => c.name === channel);
+
+    const em = new MessageEmbed()
+      .setColor(this.color)
+      .setDescription(this.logMessage.replace("%mention%", message.author.id).replace("%channel%", message.channel.id))
+      .setTimestamp();
+
+    this.channel.send({embeds: [em]});
+  }
+}
+
